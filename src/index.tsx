7fd6ae923e56cba9701353209c33e9db7159157e
Matt Campbell <mattcampbell@pobox.com>
2021-03-19 14:58:26 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "normalize.css/normalize.css"
+import "./site.css"
+
+/*
+ReactDOM.render(
+  <AppLocalizationProvider>
+    <App />
+  </AppLocalizationProvider>,
+  document.getElementById("root")
+)
+*/
